b74c82c12afc68432276ef800ba3e5cb3b0e62c3
Vinay Kumar S <vinaykumar68660340161@gmail.com>
2026-03-22 21:44:52 +0530

    Add files via upload

---
+++ 4conditionalloops.py/whileloop.py
@@ -0,0 +1,24 @@
+a=1
+while(a<=10):
+     x=0
+     while(x<a):
+          print("chandan",end=" ")
+          x=x+1
+     print()
+     a=a+1
+goal=input("Enter your goal:")
+goal=goal.lower()
+while(goal==goal):
+    goal=input("if you complete your task type (yes) no(type your goal): ")
+    goal=goal.lower()
+    if(goal=="yes"):
+        print("Congrats your moving to 1% group")
+        break
+   
+i=1
+
+while(i<=10):
+    print(i)
+    i=i+1
+   
+    
