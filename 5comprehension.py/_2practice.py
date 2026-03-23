ff04a44383279eb3c50ef0ddda45ff74d5965276
Vinay Kumar S <vinaykumar68660340161@gmail.com>
2026-03-23 22:00:06 +0530

    Add files via upload

--- 5comprehension.py/_2practice.py
+++ 5comprehension.py/_2practice.py
@@ -1,3 +1,19 @@
 #list comprehension
-ml=[print(i) for i in range(1,11)]
-
+ml=[i**2 for i in range(1,11)]#squares of list  numbers
+print(ml)
+ml=[i for i in range(1,11) if i%2==0]
+print(f"even list {ml}")
+w=["Apple","bannana","graphes"]#converting upper case
+w=[i.upper() for i in w]
+print(w)
+#create new list greather than 10
+populationlist=[10,11,25,67,43,24,8,5]
+populationlist=[i for i in populationlist if(i>10)]
+print(f"population greather than 10:{populationlist}")
+L=[]
+for i in range(1,11):
+    if(i%2==0):
+        L.append("even")
+    else:
+        L.append("odd")
+print(L)
---
+++ 5comprehension.py/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+#list comprehension
+ml=[i**i for i in range(1,11)]
+print(ml)
